9c06253d2a6f956a14c8bc7133f7228b65ef38a1
admivideo <42650731+admivideo@users.noreply.github.com>
2025-08-30 23:04:45 +0200

    Move logout to profile page and drop home link

--- header.php
+++ header.php
@@ -23,11 +23,9 @@ if (session_status() === PHP_SESSION_NONE) {
     <nav>
         <button class="menu-toggle" aria-label="Menú"><span></span><span></span><span></span></button>
         <ul class="menu">
-            <li><a href="/panel.php">Inicio</a></li>
             <?php if(isset($_SESSION['user_id'])): ?>
                 <li><a href="/tableros.php">Tableros</a></li>
                 <li><a href="/cpanel.php"><?= htmlspecialchars($_SESSION['user_name'] ?? 'Usuario'); ?></a></li>
-                <li><a href="/logout.php">Salir</a></li>
             <?php else: ?>
                 <li><a href="/login.php">Login</a></li>
                 <li><a href="/register.php">Registro</a></li>
