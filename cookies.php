a8f41a2f17fa27fe87cade5ea9deb8094e3b595b
admivideo <42650731+admivideo@users.noreply.github.com>
2025-09-04 00:08:08 +0200

    chore: add legal and about pages content

--- cookies.php
+++ cookies.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
-<h1>Cookies</h1>
-<p>Información sobre el uso de cookies en el sitio.</p>
+<h1>Uso de cookies</h1>
+<p>linkaloo utiliza cookies propias y de terceros para recordar tus preferencias, medir el uso del sitio y mejorar nuestros servicios.</p>
+<p>Al continuar navegando aceptas su uso. Puedes obtener más información o gestionar tus preferencias en nuestra <a href="/politica_cookies.php">Política de cookies</a>.</p>
 </div>
 </body>
 </html>
-
